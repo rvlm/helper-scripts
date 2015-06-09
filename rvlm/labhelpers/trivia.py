f6f76d2c607bb58606df26bdf1cd7462ac411ab1
Pavel Kretov <firegurafiku@gmail.com>
2015-06-09 20:22:37 +0300

    trivia.py: add a bit of documentation
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- rvlm/labhelpers/trivia.py
+++ rvlm/labhelpers/trivia.py
@@ -1,2 +1,11 @@
-def omit_empty_items(lst):
-    return filter(lambda x: x != "", lst)
+
+def omit_empty_items(sequence):
+    """
+    Filters out sequence entries which are :const:`None` or empty. If argument
+    is :const:`None` than the return value is :const:`None` too, but if argument
+    is empty an empty sequence, another empty sequence is returned.
+    """
+    if sequence is None:
+        return None
+    else:
+        return filter(lambda x: x (is not None) and (len(x) != 0), sequence)
