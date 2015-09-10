13159812c8022f5054c8e3787f246042871e420a
Pavel Kretov <firegurafiku@gmail.com>
2015-09-10 10:43:55 +0300

    Try PyCharm for documenting and refactoring
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- rvlm/labhelpers/extdict.py
+++ rvlm/labhelpers/extdict.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+rvlm.labhelpers.extdict: helper routines for Python built-in dictionaries
+=========================================================================
+
+This is a helper module, extending python built-in dictionaries (or anything
+following their interface) in way the Python itself lacks for some reasons.
+Those reason may be different: the routines may be not very pythonic by their
+nature, nobody thought that's a good idea to add something like them into the
+standard library, or anything.
+
+For example, this module has `mapk` and `mapv` functions which help avoiding
+unnecessary loops over dictionary in the code, while pure pythonostas prefer
+dictionary comprehension instead. Moreover, their names are not very pythonic
+too, but they're short and concise, and do their work.
+
+:copyright: 2015, Pavel Kretov
+:license: MIT
+"""
 import re
 from rvlm.labhelpers.trivia import tuple1
 
@@ -31,14 +49,14 @@ def mapv(f, dic):
 
 def extract_keys(dic, pattern, keytypes=float):
     """
-    Rebuilds dictionary by extracting numbers from its keys. This function tries
-    to match each key (which must be a string value) against given `pattern`.
-    Then all matched items are gathered into another dictionary, with string
-    keys converted into number tuples.
+    Rebuilds dictionary by extracting numbers from its keys. This function
+    tries to match each key (which must be a string value) against given
+    `pattern`. Then all matched items are gathered into another dictionary,
+    with string keys converted into number tuples.
 
     Pattern should contain at least one `{}` placeholder, denoting a number to
-    be extracted. If two or more placeholders are given, they are extracted into
-    a tuple. See some examples:
+    be extracted. If two or more placeholders are given, they are extracted
+    into a tuple. See some examples:
 
         ...
 
--- rvlm/labhelpers/trivia.py
+++ rvlm/labhelpers/trivia.py
@@ -1,14 +1,31 @@
+"""
+.. coding: utf-8
+
+`rvlm.labhelpers.trivia`: trivial things Python lacks
+=====================================================
+
+This is a helper module, providing some trivial, but still useful and
+convenient helper functions, which are absent from the standard library for
+some reason. Not all of them may be a good idea, thought, and none of them
+claims to be the pythonic™ way to do things.
+
+Many of the module function try to resemble things widely used in functional
+languages, like :func:`head`.
+
+:copyright: 2015, Pavel Kretov
+:license: MIT
+"""
+
 
 def omit_empty_items(sequence):
     """
-    Filters out sequence entries which are :const:`None` or empty. If argument
-    is :const:`None` than the return value is :const:`None` too, but if argument
-    is empty an empty sequence, another empty sequence is returned.
+    Filters out sequence items which are :const:`None` or empty. If argument is
+    :const:`None` than the return value is :const:`None` too, but if argument
+    is an empty sequence, another empty sequence is returned.
     """
-    if sequence is None:
-        return None
-    else:
-        return filter(lambda x: (x is not None) and (len(x) != 0), sequence)
+    return None if sequence is None \
+        else filter(lambda x: (x is not None) and (len(x) != 0), sequence)
+
 
 def tuple1(lst):
     """
@@ -22,16 +39,56 @@ def tuple1(lst):
     if len(ts) == 1: return ts[0]
     return ts
 
-def head(lst):
+
+def head(sequence):
     """
+    Returns first item from `sequence` or :cons:`None` if sequence is empty.
     """
     try:
-        return iter(lst).next()
+        return iter(sequence).next()
     except StopIteration:
         return None
 
-def applyfs(fs, args):
-    return map(lambda f: f(*args), fs)
 
-def unpackf(f):
-    return (lambda args: f(*args))
+def applyfs(funcs, args):
+    """
+    Applies several functions to single set of arguments. This function takes
+    a list of functions, applies each to given arguments, and returns the list
+    of obtained results. For example:
+
+        >>> from operator import add, sub, mul, div
+        >>> applyfs([add, sub, mul, div], (10, 2))
+        [12, 8, 20, 5]
+
+    :param funcs: List of functions.
+    :param args:  List or tuple of arguments to apply to each function.
+    :return:      List of results, returned by each of `funcs`.
+    """
+    return map(lambda f: f(*args), funcs)
+
+
+def unpackf(func):
+    """
+    Wraps `func` with argument unpacker. Thus the function can be called with
+    all its arguments combined into a single list. It's easier to explain this
+    by example:
+
+        >>> def f(x, y, z): return x + y + z
+        >>> unpackf(f)([1, 2, 3])
+        6
+
+    This feature is particularly useful with `numpy.apply_along_axis` function
+    which expect its function to take a single list as argument.
+
+        >>> import numpy
+        >>> import operator
+        >>> a = numpy.array([[1, 2], [3, 4]])
+        >>> numpy.apply_along_axis(sum, 0, a)
+        array([4, 6])
+        >>> numpy.apply_along_axis(unpackf(operator.add), 0, a)
+        array([4, 6])
+
+    :param func: Callable object, taking a number of positional parameters.
+    :return:     Wrapper function, taking a single list parameter.
+    """
+    return (lambda args: func(*args))
