2d2b438cd20423a30c1ababe33b23e1c8079de35
Pavel Kretov <firegurafiku@gmail.com>
2015-09-12 19:16:00 +0300

    Rename 'antennas' to 'dielenses'
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- src/rvlm/labhelpers/dielenses/distributions.py
+++ src/rvlm/labhelpers/dielenses/distributions.py
@@ -1,8 +1,11 @@
 import math
 import numpy
-import rvlm.labhelpers.extnumpy as extnumpy
+from rvlm.labhelpers.tabular  import tabularmf
+from rvlm.labhelpers.extnumpy import filter_along_axis
 
-def spherical_wavefront_lense(eps0, R, angle, num, realistic=True, omit_outer_points=True):
+
+def spherical_wavefront_lens(eps0, R, angle, num,
+                             realistic=True, omit_outer_points=True):
     """
     Generates a table describing the distribution of epsilon (material
     permittivity) in space in order to compensate spherically curved wavefront
@@ -15,22 +18,20 @@ def spherical_wavefront_lense(eps0, R, angle, num, realistic=True, omit_outer_po
     :param realistic:
     :param omit_outer_points:
     """
-    def epsilon(x,y,z):
+    def epsilon(x, y, z):
         result = eps0 * z*z / (x*x + y*y)
         if realistic and result < 1.0:
             result = 1.0
 
         return result
-            
+
     grid = numpy.meshgrid(numpy.linspace(-R, R, num=num),
                           numpy.linspace(-R, R, num=num),
                           numpy.linspace( 0, R, num=num))
 
-    result = extnumpy.tabularf(grid, [f])
-
+    result = tabularmf(grid, [epsilon])
     if omit_outer_points:
-        pred = lambda (x,y,z,_): abs(x*x + y*y)/(z*z) < math.tan(angle)**2
-        result = extnumpy.filter_along_axis(pred, 1, result)
+        cutoff = lambda (x,y,z,_): abs(x*x + y*y)/(z*z) < math.tan(angle)**2
+        result = filter_along_axis(cutoff, 1, result)
 
     return result
-    
