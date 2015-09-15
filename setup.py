176f740582604af430af22e3ccc4230e7d1af211
Pavel Kretov <firegurafiku@gmail.com>
2015-09-15 16:34:14 +0300

    setup.py: remove unit tests configuration
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- setup.py
+++ setup.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
-from setuptools import setup
-from setuptools.command.test import test as TestCommand
 import os
-import sys
+from setuptools import setup
 
 
-# Utility function to read whole file contents.
-def slurp(fname):
-    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+def slurp(filename):
+    """
+    Return whole file contents as string. File is searched relative to
+    directory where this `setup.py` is located.
+    """
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
         return f.read()
 
-
-# Helpful class for pytest integration. It was taken without modifications
-# from https://pytest.org/latest/goodpractises.html.
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args  = []
-        self.test_suite = True
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.test_args)
-        if errno != 0:
-            sys.exit(errno)
 
 setup(
     name               = "rvlm.labhelpers",
@@ -36,8 +24,6 @@ setup(
     url                = "https://github.com/rvlm/rvlm-labhelpers",
     keywords           = ["helpers"],
     requires           = ["numpy", "rvlm.entrypoint"],
-    tests_require      = ['pytest'],
-    cmdclass           = {'test': PyTest},
     description        = ("Helper library and scripts which were useful for "
                           "my experiments at the lab #426 (devoted to UWB "
                           "signals and antennas) during my PhD at Voronezh "
