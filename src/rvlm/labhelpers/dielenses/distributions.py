a8b9b3c04e6bc6ea49fff02b2ee0a5f466f83211
Pavel Kretov <firegurafiku@gmail.com>
2015-09-15 16:35:06 +0300

    distributions.py: minor refactoring and documenting
    Signed-off-by: Pavel Kretov <firegurafiku@gmail.com>

--- src/rvlm/labhelpers/dielenses/distributions.py
+++ src/rvlm/labhelpers/dielenses/distributions.py
@@ -11,12 +11,12 @@ def spherical_wavefront_lens(eps0, R, angle, num,
     permittivity) in space in order to compensate spherically curved wavefront
     in a conic antenna.
 
-    :param eps0:
-    :param R:
-    :param angle:
-    :param num:
-    :param realistic:
-    :param omit_outer_points:
+    :param eps0:  Maximum available permittivity to use.
+    :param R:     Radius of the wavefront to compensate.
+    :param angle: Conic angle to cut dielectric outside.
+    :param num:   Number of points in the generated mesh (along each axis).
+    :param realistic:         Do not generate materials with epsilon < 1.
+    :param omit_outer_points: Do not generate points outside of conic `angle`.
     """
     def epsilon(x, y, z):
         result = eps0 * z*z / (x*x + y*y)
@@ -25,13 +25,15 @@ def spherical_wavefront_lens(eps0, R, angle, num,
 
         return result
 
+    def cutoff(x, y, z, _):
+        return abs(x*x + y*y)/(z*z) < math.tan(angle)**2
+
     grid = numpy.meshgrid(numpy.linspace(-R, R, num=num),
                           numpy.linspace(-R, R, num=num),
                           numpy.linspace( 0, R, num=num))
 
     result = tabularmf(grid, [epsilon])
     if omit_outer_points:
-        cutoff = lambda (x,y,z,_): abs(x*x + y*y)/(z*z) < math.tan(angle)**2
         result = filter_along_axis(cutoff, 1, result)
 
     return result
